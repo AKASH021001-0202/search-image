d025a1e71ab2b960e4ca7375ff1ecb864c3833b8
AKASH021001-0202 <nakashn007@gmail.com>
2024-03-08 16:35:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById('search-btn').addEventListener('click', async () => {
+    const searchTerm = document.getElementById('inp-search').value.trim();
+    const url = 'https://api.freepik.com/v1/resources?locale=en-US&page=1&limit=10&order=latest&term=' + encodeURIComponent(searchTerm);
+    const apiKey = 'FPSXc393a6224f5348afa7da650c8d65be68';
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept-Language': 'en-US',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-Freepik-API-Key': apiKey
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        displayResults(data.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
+
+function displayResults(images) {
+    const imageResults = document.getElementById('image-results');
+    imageResults.innerHTML = ''; // Clear previous results
+    images.forEach(image => {
+        const imageElement = document.createElement('img');
+        imageElement.src = image.image.source.url;
+        imageElement.alt = image.title;
+        imageResults.appendChild(imageElement);
+    });
+}
